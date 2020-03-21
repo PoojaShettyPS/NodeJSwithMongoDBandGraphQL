6d99588644d94728180640b57b84eb82c10790d6
Parthiban Subramanian <parthiban.subramanian@mobinius.com>
2020-03-21 14:30:53 +0530

    Done | Date Filter Schema Change

--- config/components/mongodb.js
+++ config/components/mongodb.js
@@ -1,10 +1,10 @@
-let config = {    
-    uri: "mongodb://192.168.43.175:27017/MeetingDB",
+let config = {
+    uri: "mongodb://localhost:27017/MeetingDB",
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     }
-} 
+}
 
 module.exports = config
--- web/router/api/meeting/resolvers.js
+++ web/router/api/meeting/resolvers.js
@@ -1,92 +1,88 @@
     const path = require('path'),
-    meetingModel = require(path.resolve('.') + '/models/mongodb').meeting;
+        meetingModel = require(path.resolve('.') + '/models/mongodb').meeting;
 
-const resolvers = {
-    Query: {
-        getsinglemeeting: async (root, args, context, info) => {
-            console.log(args);
-            return await meetingModel.findOne({
-                _id: args._id,
-            }).then((result) => {
-                console.log(result);
-                return result;
-            }).catch((error) => {
-                console.log(error);
-                return error.message;
-            });
+    const resolvers = {
+        Query: {
+            getsinglemeeting: async (root, args, context, info) => {
+                console.log(args);
+                return await meetingModel.findOne({
+                    _id: args._id,
+                }).then((result) => {
+                    return result;
+                }).catch((error) => {
+                    return error.message;
+                });
+            },
+            getmeetingsbytime: async (root, args, context, info) => {
+                const fromMeetingTime = new Date(args.fromTime);
+                const toMeetingTime = new Date(args.toTime);
+                return await meetingModel.aggregate([{
+                    $match: {
+                        $or: [{
+                                fromTime: {
+                                    $gte: fromMeetingTime,
+                                    $lte: toMeetingTime,
+                                }
+                            },
+                            {
+                                toTime: {
+                                    $lte: toMeetingTime,
+                                    $gte: fromMeetingTime
+                                }
+                            }
+                        ]
+                    }
+                }]).then((result) => {
+                    return result;
+                }).catch((error) => {
+                    return error.message;
+                });
+            },
+            meetings: async (root, {}) => {
+                return await meetingModel.find({});
+            },
+            listMeetings: async (root, {
+                filter
+            }) => {
+                if (filter && filter.searchText) {
+                    let searchText = filter.searchText.toLowerCase();
+                    return courses.filter(x => x.title.toLowerCase().includes(searchText) ||
+                        x.description.includes(searchText));
+                } else {
+                    return courses;
+                }
+            }
         },
-        getmeetingsbytime: async(root, args, context, info) => {    
-            console.log(new Date(args.fromTime));
-            console.log(args.fromTime);
-            console.log(new Date(args.toTime));
-            const fromMeetingTime = new Date(args.fromTime);
-            const toMeetingTime = new Date(args.toTime);
-            return await meetingModel.aggregate([{  
-                $match : {      
-                       $or : [{
-                           fromTime : {
-                               $gte : fromMeetingTime,
-                               $lte : toMeetingTime,
-                           }},
-                           {
-                               toTime : {
-                                   $lte : toMeetingTime,
-                                   $gte : fromMeetingTime 
-                               }
-                           }]
-                        }
-                       }]).then((result) => {
-                console.log(result);
-                return result;
-            }).catch((error) => {
-                console.log(error);
-                return error.message;
-            });
-        },
-        meetings: async (root, { }) => {
-            return await meetingModel.find({});
-        },
-        listMeetings: async (root, { filter }) => {
-            console.log('filter------->', JSON.stringify(filter));
-            if (filter && filter.searchText) {
-                let searchText = filter.searchText.toLowerCase();
-                return courses.filter(x => x.title.toLowerCase().includes(searchText) ||
-                    x.description.includes(searchText));
-            } else {
-                return courses;
+        Mutation: {
+            createMeeting: (root, args, context, info) => {
+                console.log(args);
+                let meetingData = new meetingModel(args);
+                meetingData.save();
+                return true;
+            },
+            updateMeeting: (root, args, context, info) => {
+                console.log(args);
+                meetingModel.update({
+                    _id: args._id
+                }, {
+                    $set: args
+                }).then((result) => {
+                    return result;
+                }).catch((error) => {
+                    return error.message;
+                });
+            },
+            deleteMeeting: (root, args, context, info) => {
+                console.log(args);
+                meetingModel.deleteOne({
+                    _id: args._id
+                }).then((result) => {
+                    return result;
+                }).catch((error) => {
+                    return error.message;
+                });
             }
         }
-    },
+    };
 
-    Mutation: {
-        createMeeting: (root, args, context, info) => {
-            let meetingData = new meetingModel(args);
-            meetingData.save();
-            return true;
-        },
-        updateMeeting: (root, args, context, info) => {
-            console.log(args);
-            meetingModel.update({
-                _id: args._id
-            }, {
-                $set: args
-            }).then((result) => {
-                return result;
-            }).catch((error) => {
-                return error.message;
-            });
-        },
-        deleteMeeting: (root, args, context, info) => {
-            console.log(args);
-            meetingModel.deleteOne({
-                _id: args._id
-            }).then((result) => {
-                return result;
-            }).catch((error) => {
-                return error.message;
-            });
-        }
-    }
-};
-
-module.exports = resolvers;
+    module.exports = resolvers;
